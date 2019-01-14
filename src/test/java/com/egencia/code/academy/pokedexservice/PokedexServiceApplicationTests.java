0572e826d5061da902383c4f57bef2ebd20aef58
chgerber <chgerber@egencia.com>
2019-01-14 16:41:46 +0100

    Spring boot init

---
+++ src/test/java/com/egencia/code/academy/pokedexservice/PokedexServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.egencia.code.academy.pokedexservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PokedexServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
