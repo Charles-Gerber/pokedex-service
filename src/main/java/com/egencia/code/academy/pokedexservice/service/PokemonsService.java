fba9e4c3dc022d1b9505affc7dd470809b1de0b1
chgerber <chgerber@egencia.com>
2019-01-28 13:24:13 +0100

    reset model and service to begin exercise

--- src/main/java/com/egencia/code/academy/pokedexservice/service/PokemonsService.java
+++ src/main/java/com/egencia/code/academy/pokedexservice/service/PokemonsService.java
@@ -4,26 +4,18 @@ import com.egencia.code.academy.pokedexservice.model.Pokemon;
 import com.egencia.code.academy.pokedexservice.model.PokemonDetail;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class PokemonsService {
 
     public List<Pokemon> getAll() {
-        List<Pokemon> pokemons = new ArrayList<>();
-        pokemons.add(new Pokemon(1L,"Bulbasaur", Arrays.asList("poison", "grass"), 0));
-        pokemons.add(new Pokemon(2L,"Ivysaur", Arrays.asList("air"), 1));
-        return pokemons;
+        // TODO : exercie 1 : retourner une liste de pokemon
+        return null;
     }
 
     public PokemonDetail getDetail(Integer id) {
-        PokemonDetail pokemonDetail = new PokemonDetail(1L,"Bulbasaur", Arrays.asList("poison", "grass"), 0);
-        pokemonDetail.setImage("https://www.pokebip.com/pokedex-images/artworks/1.png");
-        pokemonDetail.setDescription("blbablalba");
-        pokemonDetail.setWeight(1.2);
-        pokemonDetail.setHeight(34.5);
-        return pokemonDetail;
+        // TODO : exercice 2 : retourner le détail d'un pokemon (il faut déjà le trouver dans la liste)
+        return null;
     }
 }
